e0bf9fff1a4943b609fec538672f1bce0e4565f2
radithya-mahesa <radithya.syabil.com>
2024-08-05 15:16:05 +0700

    refractor : change db name

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,8 +1,9 @@
 <?php
+//localost
     $hostname = "127.0.0.1";
     $username = "root";
     $password = "";
-    $database = "perpusRadit";
+    $database = "perpusradit";
 
     $db = mysqli_connect($hostname, $username, $password, $database) 
     or die("koneksi malas menyambung");
